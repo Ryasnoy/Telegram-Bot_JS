467596d2e144bae6cdb04c55e05975b8e1533a05
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-05-10 17:51:54 +0300

    Update Message/Event

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const TOKEN = '460496377:AAEfoxLCOmoMGVXgsrvXnWp7C_qcRtQb2Nw'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('new_chat_members', msg => {
-  bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}! Расскажи немного о себе, что бы мы с тобой познакомились поближе`)
+bot.on('new_chat_members', message => {
+  bot.sendMessage(message.chat.id, `Привет, ${message.new_chat_member.username}! Расскажи немного о себе, что бы мы с тобой познакомились поближе`)
 })
